21c64317d0d83676e7e6efe05de6fff864b0b9b5
skanga <shiraz.kanga@oracle.com>
2021-04-25 14:45:10 -0700

    Fix wrapper scripts

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # Levels are ERROR, WARN, INFO, DEBUG, TRACE
 export LOGBACK_LOG_LEVEL=WARN
-java --add-opens java.base/jdk.internal.misc=ALL-UNNAMED -Dio.netty.tryReflectionSetAccessible=true -Dnashorn.args=--no-deprecation-warning --illegal-access=warn -Djava.net.preferIPv4Stack=true -jar target/netty-socks5-1.0.jar &
+java --add-opens java.base/jdk.internal.misc=ALL-UNNAMED -Dio.netty.tryReflectionSetAccessible=true -Dnashorn.args=--no-deprecation-warning --illegal-access=warn -Djava.net.preferIPv4Stack=true -jar target/padded-socks-1.0.jar &
 # wget -N http://wpad/wpad.dat
-java -cp target/netty-socks5-1.0.jar paddedsocks.SocksGet http://wpad/wpad.dat localhost 1080 wpad.dat
+java -cp target/padded-socks-1.0.jar paddedsocks.SocksGet http://wpad/wpad.dat localhost 1080 wpad.dat
 echo done
--- run8.bat
+++ run8.bat
@@ -1,7 +1,7 @@
 
 :: Levels are ERROR, WARN, INFO, DEBUG, TRACE
 set LOGBACK_LOG_LEVEL=WARN
-start java -Djava.net.preferIPv4Stack=true -jar target/netty-socks5-1.0.jar
+start java -Djava.net.preferIPv4Stack=true -jar target\padded-socks-1.0.jar
 ::wget -N http://wpad/wpad.dat
-java -cp target/netty-socks5-1.0.jar nettysocks.SocksGet http://wpad/wpad.dat localhost 1080 wpad.dat
+java -cp target\padded-socks-1.0.jar paddedsocks.SocksGet http://wpad/wpad.dat localhost 1080 wpad.dat
 echo done
